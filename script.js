d222a15412a95e58e2a63e7b5e74ea1ee8fe78ad
Matthew Garcia <matthew27garcia@gmail.com>
2023-08-21 15:46:15 -0500

    added structure

---
+++ script.js
@@ -0,0 +1,33 @@
+function toggleSideNav(clickedElemnt) {
+    clickedElemnt.classList.toggle("change");
+    document.getElementById("mySideNav").classList.toggle("show-nav");
+    document.querySelector(".overlay").classList.toggle("show");
+    document.getElementById("mySideNav").classList.toggle("hide-nav");
+    document.body.classList.toggle("no-scroll");
+  }
+  
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
