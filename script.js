e963151d81bc73bd764bb4f47352902e00d73e73
Matthew Garcia <matthew27garcia@gmail.com>
2023-08-22 17:13:08 -0500

    several changes

--- script.js
+++ script.js
@@ -5,29 +5,6 @@ function toggleSideNav(clickedElemnt) {
     document.getElementById("mySideNav").classList.toggle("hide-nav");
     document.body.classList.toggle("no-scroll");
   }
-  
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+let myBtn = document.getElementById('myBtn').addEventListener('click')
