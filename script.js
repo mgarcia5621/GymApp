42016ab12d5a7093f332d1a4822e5f1bdf22dc6f
Matthew Garcia <matthew27garcia@gmail.com>
2023-08-25 20:04:31 -0500

    added the modal

--- script.js
+++ script.js
@@ -7,4 +7,30 @@ function toggleSideNav(clickedElemnt) {
   }
 
 
-let myBtn = document.getElementById('myBtn').addEventListener('click')
+// let myBtn = document.getElementById('myBtn').addEventListener('click')
+
+
+var mpopup = document.getElementById('mpopupBox');
+
+// Select trigger link
+var mpLink = document.getElementById("mpopupLink");
+
+// Select close action element
+var close = document.getElementsByClassName("close")[0];
+
+// Open modal once the link is clicked
+mpLink.onclick = function() {
+    mpopup.style.display = "block";
+};
+
+// Close modal once close element is clicked
+close.onclick = function() {
+    mpopup.style.display = "none";
+};
+
+// Close modal when user clicks outside of the modal box
+window.onclick = function(event) {
+    if (event.target == mpopup) {
+        mpopup.style.display = "none";
+    }
+};
